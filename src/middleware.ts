3cc33858184177fc68809a57a45e4043c7ed54fb
kims07231992 <kims07231992@gmail.com>
2025-08-05 22:00:04 -0700

    Update favicon

--- src/middleware.ts
+++ src/middleware.ts
@@ -154,6 +154,6 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    "/((?!api|_next/static|_next/image|favicon.ico|images|assets|png|svg|jpg|jpeg|gif|webp).*)",
+    "/((?!api|_next/static|_next/image|favicon.ico|favicon.svg|images|assets|png|svg|jpg|jpeg|gif|webp).*)",
   ],
 }
